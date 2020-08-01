fbd0ea998733e83e1ac2c3795069f405a2deb12d
Tarcisio Silva <tarcisior19@hotmail.com>
2020-08-01 20:17:56 -0300

    feat : Implementação da tela de Not Fould

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import img404 from './../../Images/seo-e-web.png'
 
 import { Area } from './styled'
 
 export default () => {
     return(
-        <h2>Pagina não encontrada 404</h2>
+        <Area>
+            <div className="div-box">
+                <img src={img404} alt="" />
+                <div className="div-box-text">
+                    <p>Desculpe</p>
+                    <p>Página não econtrada</p>
+                    <Link to="/" >Voltar para a página inicial!</Link>
+                </div>
+            </div>
+        </Area>
     )
 }
