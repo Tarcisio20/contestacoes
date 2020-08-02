1514b023219e1ee932c5f31fdb82d884ee2bf400
Tarcisio Silva <tarcisior19@hotmail.com>
2020-08-02 13:21:45 -0300

    feat : Implementação das funcionalidades de login

---
+++ src/Helpers/API.js
@@ -0,0 +1,9 @@
+const API = {
+    login:async (email, password) => {
+
+
+        return {"error":"Funcionalidade incompleta"}
+    }
+}
+
+export default ()=>API
--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,25 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BoxMessageError } from './../../Components/mainComponents'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ErrorIcon from '@material-ui/icons/Error';
-
+import { doLogin } from './../../Helpers/AuthHandler'
+import useApi from './../../Helpers/API'
 import { Area } from './styled'
 
 export default props => {
+    const api = useApi()
+    //States
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [rememberPassowrd, setRememberPassword] = useState(false)
+    const [disabled, setDisabled] =useState(false)
+    const [error, setError] = useState('')
+
+    // FUNCTIONS
+    const handlerSubmit = async e => {
+        e.preventDefault()
+        setDisabled(true)
+
+        const json = await api.login(email, password)
+
+        if(json.error) {
+            setError(json.error)
+        }else {
+            doLogin(json.token, rememberPassowrd)
+            window.location.href = '/'
+        }
+
+
+
+    }
+
     return (
         <Area>
-            <BoxMessageError>Mensagem de erro <ErrorIcon /></BoxMessageError>
-            <form>
+           {error && <BoxMessageError>{error} <ErrorIcon /></BoxMessageError>
+            }
+            <form onSubmit={handlerSubmit}>
                 <div className="div-form-input">
                     <label>Login:</label>
-                    <input type="email" />
+                    <input type="email"
+                    required
+                    disabled={disabled}
+                    value={email}
+                    onChange={e=>setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="div-form-input">
                     <label>Senha:</label>
-                    <input type="password" />
+                    <input type="password"
+                    required
+                    disabled={disabled}
+                    value={password}
+                    onChange={e=>setPassword(e.target.value)}
+                    />
                 </div>
                 <div className="div-form-input div-diferent">
-                    <input type="checkbox" />
+                    <input type="checkbox"
+                    disabled={disabled}
+                    checked={rememberPassowrd}
+                    onChange={()=>setRememberPassword(!rememberPassowrd)}
+                    />
                     <label className="text-diferent">Lembrar minha senha.</label>
                 </div>
                 <div className="div-form-input">
